f7cc4c3138683a4247f4b80062071494bdefd4d3
JaznanOfficial <jaznanofficial@gmail.com>
2022-09-03 19:26:46 +0600

    random and all data getting method done

--- index.js
+++ index.js
@@ -1,34 +1,60 @@
-const { json } = require('express');
-const express = require('express')
-const PORT = 5000 || 'https://node-mongo-assignment-1.vercel.app/';
-const app = express()
-const fs = require('fs')
-const http = require('http')
+const { json } = require("express");
+const express = require("express");
+const PORT = 5000 || "https://node-mongo-assignment-1.vercel.app/";
+const app = express();
+const fs = require("fs");
+const http = require("http");
 // const userRoute = require('./routes/usersRoute')
 // const defaultRoute = require('./routes/defaultRoute')
-
 
 // app.use('/', defaultRoute)
 // app.use('/user', userRoute)
 // app.all('*', (req, res) => {
-    // res.send('Oops, you are in wrong route. please go to right url')
+// res.send('Oops, you are in wrong route. please go to right url')
 // })
 
-
-app.get('/user/all',(req, res) => {
+app.get("/user/all", async (req, res) => {
     // console.log(req.url);
-    if (req.url == '/user/all') {
-        fs.readFile('userData.json', (err, data) => {
+    if (req.url == "/user/all") {
+        fs.readFile("userData.json", (err, data) => {
             if (err) {
-                res.write('failed to get data!!!')
-                res.end()
-            }
-            else {
-                res.write(data)
-                res.end()
-            }
-        })
-    }
-})
+                res.write("failed to get data!!!");
+                res.end();
+            } else {
+                // const buf = fs.readFileSync("userData.json");
+                // const convertedData = buf.toString("utf8");
 
-app.listen(PORT, console.log('server is running on port', PORT))
+                // console.log(convertedData);
+                res.write(data);
+                res.end();
+            }
+        });
+    }
+});
+
+app.get("/user/random", async (req, res) => {
+    console.log(req.url);
+    if (req.url == "/user/random") {
+        fs.readFile("userData.json", (err, data) => {
+            if (err) {
+                res.write("failed to get data!!!");
+                res.end();
+            } else {
+                const buf = fs.readFileSync("userData.json");
+                const decodedData = buf.toString("utf8");
+                const convertedData = JSON.parse(decodedData)
+                console.log(convertedData.length);
+                const randomNumber = Math.floor(Math.random() * convertedData.length);
+                console.log(randomNumber);
+                // console.log(data);
+                const randomData = convertedData[randomNumber];
+                const stringifiedRandomData = JSON.stringify(randomData)
+                console.log(stringifiedRandomData);
+                res.write(stringifiedRandomData);
+                res.end();
+            }
+        });
+    }
+});
+
+app.listen(PORT, console.log("server is running on port", PORT));
