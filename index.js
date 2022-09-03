c610923a7e0348ff0823d7df3eff78d664b925ea
JaznanOfficial <jaznanofficial@gmail.com>
2022-09-03 10:10:15 +0600

    a bug fixed

--- index.js
+++ index.js
@@ -3,7 +3,9 @@ const PORT = 5000;
 const app = express()
 const userRoute = require('./routes/usersRoute')
 
-
+app.use('/', (req, res) => {
+    res.send('<h1>This is node mongo assignment-1 server</h1>')
+})
 app.use('/user', userRoute)
 app.all('*', (req, res) => {
     res.send('Oops, you are in wrong route. please go to right url')
